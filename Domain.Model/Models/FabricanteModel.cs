51f770a4c10ff863c8492b4f9b1cdaef9643af8d
MariiMariis <78056802+MariiMariis@users.noreply.github.com>
2021-08-11 15:15:37 -0300

    .

---
+++ Domain.Model/Models/FabricanteModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Model.Models
+{
+    public class FabricanteModel
+    {
+        public int Id { get; set; }
+
+        public string NomeFabricante { get; set; }
+
+        public string Fundador { get; set; }
+
+        public string PaisOrigem { get; set; }
+
+        public DateTime DataFundacao { get; set; }
+    }
+}
